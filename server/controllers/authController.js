c77c4b9ca59e96f1dbfb92b8d42cfa067dde921e
abhaypal8188 <abhaypal8188@gmail.com>
2026-05-08 19:58:57 +0530

    ..

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -71,6 +71,8 @@ export const loginUser = async (req, res) => {
 export const logoutUser = (req, res) => {
   res.cookie('jwt', '', {
     httpOnly: true,
+    secure: true,
+    sameSite: 'none',
     expires: new Date(0)
   });
   res.status(200).json({ message: 'Logged out successfully' });
--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    secure: true, // Must be true for sameSite: 'none'
+    sameSite: 'none', // Required for cross-site cookies on Vercel
     maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
   });
 
